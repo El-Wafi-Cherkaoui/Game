137d89666f0417dcc23def0a6f2beba15c02ef7d
Cherkaoui El Wafi <elwafi.Cherkaoui@gmail.com>
2024-11-19 18:19:04 +0100

    verify & fill next

--- functions.js
+++ functions.js
@@ -11,4 +11,20 @@ function generate_word(){
 
     return randomWord
 }
-console.log(generate_word())
+let genrated_word = generate_word()
+let tries = 0
+function verify_answer(answer) {
+    // console.log('answer', answer);
+    if(answer == genrated_word){
+        return true
+    }
+    else{
+        false
+    }
+}
+function fill_next(correct_answer, tries) {
+    let inputs = document.querySelectorAll('input')
+    tries += 1
+    inputs[tries].value = correct_answer[correct_answer[tries]]
+}
+// verify_answer(genrated_word)

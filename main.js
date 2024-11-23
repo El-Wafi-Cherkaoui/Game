a69f90a731c8d691d47070a069fedc147d2868b1
hamdi touballi <hamditouballi2002@gmail.com>
2024-11-23 10:06:04 +0100

    add button of start

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import { generate_word, Affichage } from "./functions.js";
 
-let random_word = generate_word()
-let data = ''
-let tries_counter = 0
-console.log(random_word);
+let btn = document.createElement('button')
+btn.textContent="start"
+document.body.append(btn)
 
-Affichage(random_word, data, {tries: tries_counter})
+btn.onclick = ()=>{
+    let br = document.createElement('br');
+    document.body.append(br)
+    let random_word = generate_word()
+    let data = ''
+    let tries_counter = 0
+    console.log(random_word);
+    
+    Affichage(random_word, data, {tries: tries_counter})
 
+};

d5baf9b510fdf3ab0fb99848608e83e6248aa85e
sif eddine <sifeddineafram@gmail.com>
2024-11-23 10:13:49 +0100

    losefunc and styling

--- functions.js
+++ functions.js
@@ -18,26 +18,52 @@ function Affichage(word, data_cont, tries){
      inp.id = "c0"
      inp.value= word[0]
      document.body.append(inp)
-     
+
+     inp.setAttribute('maxlength', '1');
+     inp.style.width = '20px';
+     inp.style.marginRight = '5px';
+     inp.style.margin= '10px';
+
+ 
      for (let index = 1; index < lenghtW ; index++) {
         let inp = document.createElement('input')
         inp.id = `c${index}`
         document.body.append(inp) 
+        
+        inp.setAttribute('maxlength', '1');
+        inp.style.width = '20px';
+        inp.style.marginRight = '5px';
+        inp.style.margin= '10px';
+
      }
      let btn = document.createElement('button')
      btn.textContent="send"
+     
      btn.onclick = ()=>{
         let answer_is_correct = verify_answer(get_data(), word)
         
-        if(answer_is_correct){
+         if(word.length== tries.tries+1){
+            console.log('lost');
+            
+            youlose()    
+        }
+        else if(answer_is_correct){
             alert('congrats!')
         }
+        
         else{
             fill_next(word, tries)
         }
      }
      document.body.append(btn)
     
+
+    btn.style.backgroundColor = '#007bff';
+    btn.style.color = '#fff';             
+    btn.style.padding = '8px 16px';        
+    btn.style.border = 'none';           
+    btn.style.borderRadius = '4px';        
+    btn.style.cursor = 'pointer';
 }
 
 function generate_word(){
@@ -63,12 +89,15 @@ function verify_answer(answer, correct_answer) {
     }
 }
 function fill_next(correct_answer, tries_counter) {
-    console.log(tries_counter);
+    // console.log(tries_counter);
     let inputs = document.getElementById(`c${Number(tries_counter.tries+1)}`)
-    console.log('inputs',inputs);
     tries_counter.tries += 1
     inputs.value = correct_answer[tries_counter.tries]
 }
 
+function youlose() {
+    alert('You Lose -- No guess')
+
+}
 
 export{generate_word, Affichage}
